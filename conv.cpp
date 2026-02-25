d7eccc9d2b9adf5ab009e624a7472a17ba46cb30
MostafaShimy24 <128504739+MostafaShimy24@users.noreply.github.com>
2026-02-25 23:49:15 +0200

    Update conv.cpp
    the update that worked with me

--- conv.cpp
+++ conv.cpp
@@ -4,6 +4,7 @@
 #define INDEX_WIDTH 6
 
 void conv(
+    bool ap_rst,                  // ACTIVE LOW reset
     bool write_en,
     ap_uint<INDEX_WIDTH> write_index,
     ap_int<8> x_in,
@@ -14,6 +15,7 @@ void conv(
     bool &done
 )
 {
+#pragma HLS INTERFACE ap_none port=ap_rst
 #pragma HLS INTERFACE ap_none port=write_en
 #pragma HLS INTERFACE ap_none port=write_index
 #pragma HLS INTERFACE ap_none port=x_in
@@ -23,70 +25,59 @@ void conv(
 #pragma HLS INTERFACE ap_none port=result
 #pragma HLS INTERFACE ap_none port=done
 #pragma HLS INTERFACE ap_ctrl_none port=return
+#pragma HLS PIPELINE II=1
 
     static ap_int<8> x_mem[N];
     static ap_int<8> w_mem[N];
 
-#pragma HLS ARRAY_PARTITION variable=x_mem cyclic factor=8
-#pragma HLS ARRAY_PARTITION variable=w_mem cyclic factor=8
-
+    static ap_uint<1> state = 0;     // 0=IDLE, 1=COMPUTE
+    static ap_uint<6> comp_index = 0;
     static ap_int<32> acc = 0;
-    static ap_uint<INDEX_WIDTH> comp_index = 0;
     static bool done_r = false;
-    static ap_uint<1> state = 0;
 
-    // ===== IDLE =====
-    if (state == 0)
+    // =============================
+    // ACTIVE LOW RESET
+    // =============================
+    if (ap_rst == 0)
     {
-        if (write_en && write_index < N)
-        {
-            x_mem[write_index] = x_in;
-            w_mem[write_index] = w_in;
-        }
-
-        if (clear_done)
-            done_r = false;
-
-        if (start_r)
-        {
-            acc = 0;
-            comp_index = 0;
-            state = 1;
-        }
+        state = 0;
+        comp_index = 0;
+        acc = 0;
+        done_r = false;
     }
-
-    // ===== COMPUTE =====
     else
     {
-        ap_int<16> m0 = x_mem[comp_index]     * w_mem[comp_index];
-        ap_int<16> m1 = x_mem[comp_index + 1] * w_mem[comp_index + 1];
-        ap_int<16> m2 = x_mem[comp_index + 2] * w_mem[comp_index + 2];
-        ap_int<16> m3 = x_mem[comp_index + 3] * w_mem[comp_index + 3];
-        ap_int<16> m4 = x_mem[comp_index + 4] * w_mem[comp_index + 4];
-        ap_int<16> m5 = x_mem[comp_index + 5] * w_mem[comp_index + 5];
-        ap_int<16> m6 = x_mem[comp_index + 6] * w_mem[comp_index + 6];
-        ap_int<16> m7 = x_mem[comp_index + 7] * w_mem[comp_index + 7];
-
-        ap_int<32> mult_sum =
-            (ap_int<32>)m0 +
-            (ap_int<32>)m1 +
-            (ap_int<32>)m2 +
-            (ap_int<32>)m3 +
-            (ap_int<32>)m4 +
-            (ap_int<32>)m5 +
-            (ap_int<32>)m6 +
-            (ap_int<32>)m7;
-
-        acc += mult_sum;
-
-        if (comp_index == N - 8)
+        if (state == 0)  // IDLE
         {
-            done_r = true;
-            state = 0;
+            if (write_en && write_index < N)
+            {
+                x_mem[write_index] = x_in;
+                w_mem[write_index] = w_in;
+            }
+
+            if (clear_done)
+                done_r = false;
+
+            if (start_r)
+            {
+                acc = 0;
+                comp_index = 0;
+                state = 1;
+            }
         }
-        else
+        else  // COMPUTE
         {
-            comp_index += 8;
+            acc += (ap_int<32>)(x_mem[comp_index] * w_mem[comp_index]);
+
+            if (comp_index == N-1)
+            {
+                done_r = true;
+                state = 0;
+            }
+            else
+            {
+                comp_index++;
+            }
         }
     }
 
